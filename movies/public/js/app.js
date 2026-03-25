bedbd8b1db7311c11062853057abec8b2830b872
jjjjrhwm-njm <jjjjrhwm@gmail.com>
2026-03-25 18:07:15 +0300

    Create app.js

---
+++ movies/public/js/app.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const tg = window.Telegram.WebApp;
+    tg.expand(); 
+    tg.ready();
+
+    let telegramId = "guest"; 
+    if (tg.initDataUnsafe && tg.initDataUnsafe.user) {
+        telegramId = tg.initDataUnsafe.user.id;
+    }
+
+    const userStatus = await API.checkUserStatus(telegramId);
+    UI.updateUserStatus(userStatus.isVIP);
+
+    const publicMovies = await API.getPublicMovies();
+    const vipMovies = await API.getVIPMovies();
+
+    const allMovies = [...vipMovies, ...publicMovies];
+    UI.renderHero(allMovies[0]);
+    UI.renderMovies(publicMovies, 'publicMoviesGrid', false);
+    UI.renderMovies(vipMovies, 'vipMoviesGrid', true);
+});
