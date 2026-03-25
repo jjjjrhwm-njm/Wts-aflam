d2e7ce014d3f25ad49ec26cd97c37c4811184c55
jjjjrhwm-njm <jjjjrhwm@gmail.com>
2026-03-25 19:22:04 +0300

    Create env.js

---
+++ movies/config/env.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    MONGO_URI: process.env.MONGO_URI,
+    BOT_TOKEN: process.env.BOT_TOKEN,
+    ADMIN_ID: process.env.ADMIN_ID,
+    STORAGE_CHANNEL: '@nejm_njm',
+    SESSION_SECRET: process.env.SESSION_SECRET
+};
